641310e62d8498a964036e0b0425d823832efa30
wenlishi <3197066040@qq.com>
2025-07-21 21:08:57 +0800

    auth

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,10 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.api.endpoints import auth
 from app.db import database, models
 
-# 创建数据库表
-print("正在尝试创建数据库表...")
-models.Base.metadata.create_all(bind=database.engine)
-print("数据库创建成功！")
 
 app = FastAPI(title="classroom Atttention")
 
